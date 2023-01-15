4eae758b294b78e873d28693b2783094b61e30fb
Rayhana E <florayana97@hotmail.com>
2023-01-15 21:31:58 +0100

    jscode

--- api.js
+++ api.js
@@ -2,16 +2,36 @@
 // api key: 783f6de2
 
 
-var searchMovie = document.getElementById('search-box').value
-const title = document.getElementById('title').innerHTML
-const plot = document.getElementById('plot').innerHTML
-const released = document.getElementById('released').innerHTML
-const country = document.getElementById('country').innerHTML
-const actors = document.getElementById('actors').innerHTML
-const genre = document.getElementById('genre').innerHTML
-const runtime = document.getElementById('runtime').innerHTML
 
 const apiKey = '&apikey=783f6de2'
 
+async function movieData(){
+    var searchMovie = document.getElementById('search-box').value
 
-let data=  title +  plot + released + country + actors + genre + runtime 
+
+
+    const url = "https://www.omdbapi.com/?t=" + searchMovie + apiKey
+
+     
+        const response  = await fetch (url)
+        const data = await response.json()
+        console.log(data)
+
+
+     let {
+        Title, Plot, Released, Country, Actors, Genre, Runtime, Poster
+    } = data
+  document.getElementById('Title').innerHTML = Title
+  document.getElementById('Plot').innerHTML = Plot
+  document.getElementById('Released').innerHTML = Released
+  document.getElementById('Country').innerHTML = Country
+  document.getElementById('Actors').innerHTML = Actors
+  document.getElementById('Genre').innerHTML = Genre
+  document.getElementById('Runtime').innerHTML = Runtime
+  document.getElementById("Poster").src = Poster;
+  
+ 
+
+
+}
+
