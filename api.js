aee90a642821ed665fe63673c7775f6ea1b69eb5
Rayhana E <florayana97@hotmail.com>
2023-01-16 01:01:55 +0100

    recreated dynamic elements, restyle css

--- api.js
+++ api.js
@@ -4,34 +4,39 @@
 
 
 const apiKey = '&apikey=783f6de2'
+let result = document.getElementById('display-info')
 
 async function movieData(){
-    var searchMovie = document.getElementById('search-box').value
-
-
-
-    const url = "https://www.omdbapi.com/?t=" + searchMovie + apiKey
+    var searchBox = document.getElementById('search-box').value
+    
+    const url = "https://www.omdbapi.com/?t=" + searchBox + apiKey
 
      
         const response  = await fetch (url)
         const data = await response.json()
         console.log(data)
 
+    if (data.Response == "True") {
+        result.innerHTML = `
+        <div class="display">
+        <h4>  ${data.Title} <br> *** ${data.Year} ***</h4>
+        <img src=${data.Poster}>
+        <div class="info">
+        
+        <p> Plot: ${data.Plot}</p>
+        <p> Country: ${data.Country}</p>
+        <p> Runtime: ${data.Runtime}</p>
+        </div>
 
-     let {
-        Title, Plot, Released, Country, Actors, Genre, Runtime, Poster
-    } = data
-  document.getElementById('Title').innerHTML = Title
-  document.getElementById('Plot').innerHTML = Plot
-  document.getElementById('Released').innerHTML = Released
-  document.getElementById('Country').innerHTML = Country
-  document.getElementById('Actors').innerHTML = Actors
-  document.getElementById('Genre').innerHTML = Genre
-  document.getElementById('Runtime').innerHTML = Runtime
-  document.getElementById("Poster").src = Poster;
-  
- 
+        </div>
+        `
+    }
 
-
+    if(searchBox.lenght <= 0) {
+        result.innerHTML = `<p> please enter a moviee name</p>`
+    }
 }
+
+movieData()
+
 
